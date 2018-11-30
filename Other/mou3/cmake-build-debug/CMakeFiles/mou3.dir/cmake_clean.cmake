64dba9416252012ef83478bcce5a9e83829f761f
xyzyzl <albert.ye.728@gmail.com>
2018-11-29 22:42:57 -0800

    uh some files didn't come through

---
+++ Other/mou3/cmake-build-debug/CMakeFiles/mou3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mou3.dir/mou.cpp.o"
+  "mou3.pdb"
+  "mou3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mou3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gold/dec2017/piepie/cmake-build-debug/CMakeFiles/piepie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/piepie.dir/main.cpp.o"
+  "piepie.pdb"
+  "piepie"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/piepie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
