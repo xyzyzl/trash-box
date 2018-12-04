84fd944279ec98b5248ee2748896c10a8504f7e2
xyzyzl <albert.ye.728@gmail.com>
2018-12-03 22:31:10 -0800

    Lasers and Mirrors (BFS)

---
+++ gold/dec16/lasers/src/Main.java
@@ -0,0 +1,133 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+
+	private static int N, xl, yl, xb, yb;
+	private static HashMap<Line, Integer> cost;
+	private static HashMap<Integer, ArrayList<Integer> > xy, yx; // for line transitions
+	private static LinkedList<Line> q;
+	public static void main(String[] args) throws IOException
+	{
+		cost = new HashMap<Line, Integer>();
+		xy = new HashMap<Integer, ArrayList<Integer> >();
+		yx = new HashMap<Integer, ArrayList<Integer> >();
+		q = new LinkedList<Line>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream
+				(new File("lasers.in"))));
+		PrintWriter pw = new PrintWriter(new File("lasers.out"));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		xl = Integer.parseInt(st.nextToken());
+		yl = Integer.parseInt(st.nextToken());
+		xb = Integer.parseInt(st.nextToken());
+		yb = Integer.parseInt(st.nextToken());
+		q.add(new Line(xl, true));
+		q.add(new Line(yl, false));
+		cost.put(new Line(xl, true), 0);
+		cost.put(new Line(yl, false), 0);
+		for(int i = 0; i < N; i++)
+		{
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			if(!xy.containsKey(x))
+			{
+				xy.put(x, new ArrayList<Integer>());
+			}
+			xy.get(x).add(y);
+			if(!yx.containsKey(y))
+			{
+				yx.put(y, new ArrayList<Integer>());
+			}
+			yx.get(y).add(x);
+		}
+		int min = -1;
+		while(!q.isEmpty())
+		{
+			Line l = q.removeFirst();
+			// System.out.println(cost.containsKey(l));
+			// System.out.println((l.o ? 'x' : 'y') + " = " + l.c);
+			if(l.o && l.c == xb)
+			{
+				min = cost.get(l);
+				break;
+			}
+			if(!l.o && l.c == yb)
+			{
+				min = cost.get(l);
+				break;
+			}
+			if(l.o)
+			{
+				// use xy map
+				if(xy.containsKey(l.c))
+				{
+					for(int nx : xy.get(l.c))
+					{
+						Line res = new Line(nx, !l.o);
+						if(!cost.containsKey(res))
+						{
+							cost.put(res, cost.get(l) + 1);
+							q.add(res);
+						}
+					}
+				}
+			} else
+			{
+				// use yx map
+				if(yx.containsKey(l.c))
+				{
+					for(int nx : yx.get(l.c))
+					{
+						Line res = new Line(nx, !l.o);
+						if(!cost.containsKey(res))
+						{
+							cost.put(res, cost.get(l) + 1);
+							q.add(res);
+						}
+					}
+				}
+			}
+		}
+		pw.println(min);
+		pw.flush();
+		pw.close();
+	}
+
+	private static class Line
+	{
+		int c;
+		boolean o; // true = vertical, false = horizontal
+		public Line(int c, boolean o)
+		{
+			this.c=c;
+			this.o=o;
+		}
+		public int hashCode()
+		{
+			final int PRIME = 31;
+			int res = 1;
+			res = PRIME * res + (o ? 1237 : 1231);
+			res = PRIME * res + c;
+			return res;
+		}
+		public boolean equals(Object obj)
+		{
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Line other = (Line) obj;
+			if (o != other.o)
+				return false;
+			if (c != other.c)
+				return false;
+			return true;
+		}
+	}
+
+}
--- gold/mar10/balloc/main.cpp
+++ gold/mar10/balloc/main.cpp
@@ -1,23 +1,115 @@
 #include <bits/stdc++.h>
 
+#define MAXN 100005
+#define MAX (1 << 17)
+#define INF 632632632
+
 using namespace std;
 
-int N, M, c[100000];
+int N, M, c[MAXN], bit[MAX * 2], lazy[MAX * 2];
 typedef pair<int, int> pii;
 #define f first
 #define s second
-pii que[100000]; // (end, beginning)
+pii que[MAXN]; // (end, beginning)
+
+void up(int cur)
+{
+	if (cur >= MAX)
+		return;
+	bit[cur] = min(bit[2 * cur], bit[2 * cur + 1]);
+}
+
+void down(int cur)
+{
+	if (!lazy[cur])
+		return;
+	if (cur >= MAX)
+		return;
+	bit[2 * cur] -= lazy[cur];
+	bit[2 * cur + 1] -= lazy[cur];
+	lazy[2 * cur] += lazy[cur];
+	lazy[2 * cur + 1] += lazy[cur];
+	lazy[cur] = 0;
+}
+
+void init(int cur, int left, int right)
+{
+	if (cur >= 2 * MAX)
+		return;
+	down(cur);
+	int mid = left + (right - left) / 2;
+	init(2 * cur, left, mid);
+	init(2 * cur + 1, mid, right);
+	up(cur);
+}
+
+void add(int l, int r, int cur, int left, int right)
+{
+	if (left > r || right <= l)
+		return;
+	if (left >= l && right - 1 <= r)
+	{
+		lazy[cur]++;
+		bit[cur]--;
+		return;
+	}
+	down(cur);
+	int mid = left + (right - left) / 2;
+	add(l, r, 2 * cur, left, mid);
+	add(l, r, 2 * cur + 1, mid, right);
+	up(cur);
+}
+
+bool acc(int l, int r, int cur, int left, int right)
+{
+	if (left > r || right <= l)
+	{
+		cout << "phuc" << endl;
+		return true;
+	}
+	if (left >= l || right <= r + 1)
+	{
+		cout << bit[cur] << endl;
+		return (bit[cur] != 0);
+	}
+	down(cur);
+	int mid = left + (right - left) / 2;
+	bool fr = acc(l, r, 2 * cur, left, mid);
+	bool bk = acc(l, r, 2 * cur + 1, mid, right);
+	return fr && bk;
+}
+
 int main()
 {
+	ios_base::sync_with_stdio(0);
 	cin >> N >> M;
-	for(int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		cin >> c[i];
+		cin >> c[i + MAX];
 	}
-	for(int i = 0; i < M; i++)
+	for (int i = N + MAX; i < MAX * 2; i++)
 	{
-		cin >> que[i].s >> que[i].f;
+		bit[i] = INF;
 	}
-
+	init(1, 0, MAX);
+	for (int i = 0; i < M; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		que[i].f = b-1;
+		que[i].s = a-1;
+	}
+	sort(que, que + M);
+	// maintain a BIT that inserts the indices in this order
+	int ct = 0;
+	for (int i = 0; i < M; i++)
+	{
+		if (acc(que[i].s, que[i].f, 1, 0, MAX))
+		{
+			add(que[i].s, que[i].f, 1, 0, MAX);
+			ct++;
+		}
+	}
+	cout << ct << '\n';
 	return 0;
 }
